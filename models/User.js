c4d60123bc71b812d00097b06db4febd32bb2a13
prince saspara <princesaspara46@gmail.com>
2023-09-18 15:33:49 +0530

    google authentication

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new mongoose.Schema({
     username: String,
-    email: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, },
     password: String,
     name: String,
     surname: String,
